9d190f5bf1282ef8f54a81aeb6a2a9f772ab9d56
jgwilmott <60458668+jgwilmott@users.noreply.github.com>
2021-07-25 15:55:49 -0230

    Added route, made all functions begin with route

--- app.py
+++ app.py
@@ -3,5 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def route_hello_world():
     return render_template("hello.html")
+
+'''
+CAN ALSO USE:
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello World!!</p>"
+'''    
+
+@app.route("/<name>")
+def route_hello_name(name):
+    return f"<p>Hello, {name}!</p>"
